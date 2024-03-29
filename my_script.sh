95bcba703780dbc5c2c2ed766f90609146eb970e
nirupamjm <68326831+nirupamjm@users.noreply.github.com>
2024-03-29 20:21:15 +1100

    Update my_script.sh

--- my_script.sh
+++ my_script.sh
@@ -2,7 +2,7 @@
 
 # Define variables
 DOCKERFILE_PATH="./Dockerfile"
-IMAGE_NAME="my-ubuntu-image"
+IMAGE_NAME="my-centos-image"
 
 # Build Docker image
 docker build -t $IMAGE_NAME -f $DOCKERFILE_PATH .
