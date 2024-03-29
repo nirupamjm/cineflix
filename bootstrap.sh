68ebcb51fd8a33439548874407735cde411d3e70
nirupamjm <68326831+nirupamjm@users.noreply.github.com>
2024-03-29 22:56:09 +1100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+sudo apt-get update
+sudo apt-get  install openjdk-8-jdk -y
+
+mkdir streama
+cd /streama
+
+wget https://github.com/streamaserver/streama/releases/download/v1.6.1/streama-1.6.1.war
+chmod +x streama-1.6.1.war
+
+java -jar streama-1.6.1.war
